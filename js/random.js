c2b6a09b314f202e1217a409c1437d1d1f74628f
Soumitha Bhaskara <soumithaalpineti@gmail.com>
2020-07-16 13:15:44 +0530

    sorting select tag

--- js/random.js
+++ js/random.js
@@ -9,10 +9,14 @@ window.addEventListener("load", () => {
     manageLikeAndComment(document.getElementById('posters'))
     // manage commmentsform
     manageCommnetsForm(document.getElementById('addCommentForm'))
+
+    let sort = document.getElementById("sort")
+    sort.addEventListener("change",  () => {
+        sortPost(sort.value)
+    })
 })
 
 function renderDOM() {
     renderProfilePicture()
     renderPosts(Post.all().reverse(), postsDiv)
 }
-
